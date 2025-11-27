2e9fc168c52c450aec0169ff5b7de6e7fd02e502
UmerDesigns0-0 <umerandlife@gmail.com>
2025-11-27 16:50:23 +0500

    Update Socket.js

--- src/Socket.js
+++ src/Socket.js
@@ -1,7 +1,8 @@
 import io from "socket.io-client";
 
-const socket = io("https://talkify-sigma.vercel.app/", {
-    autoConnect: false
-});
+// Use VITE_SERVER_URL (set in Vercel / local .env) so we can point to the backend regardless of host
+const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
+
+const socket = io(serverUrl, { autoConnect: false });
 
 export default socket;
