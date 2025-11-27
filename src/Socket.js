d34bfadbe1a81c97991aad7ac13d18a1b354798f
UmerDesigns0-0 <umerandlife@gmail.com>
2025-11-27 16:22:44 +0500

    Update socket connection URL

--- src/Socket.js
+++ src/Socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("https://your-vercel-project.vercel.app", {
+const socket = io("https://talkify-sigma.vercel.app/", {
     autoConnect: false
 });
 
